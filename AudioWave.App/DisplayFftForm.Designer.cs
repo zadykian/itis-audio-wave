13d0afb53293a129f3d30fdff5f662f7bb1800c9
Alexander Zadykian <zadykian@outlook.com>
2022-06-08 00:47:06 +0300

    DisplayFftForm

--- AudioWave.App/DisplayFftForm.Designer.cs
+++ AudioWave.App/DisplayFftForm.Designer.cs
@@ -31,10 +31,13 @@ partial class DisplayFftForm
             this.components = new System.ComponentModel.Container();
             this.devicesComboBox = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.transformedGraphPlot = new ScottPlot.FormsPlot();
+            this.transformedFormPlot = new ScottPlot.FormsPlot();
             this.eventLoopTimer = new System.Windows.Forms.Timer(this.components);
             this.cbAutoAxis = new System.Windows.Forms.CheckBox();
             this.peakLabel = new System.Windows.Forms.Label();
+            this.windowTypeComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.originalFormPlot = new ScottPlot.FormsPlot();
             this.SuspendLayout();
             // 
             // devicesComboBox
@@ -58,22 +61,22 @@ partial class DisplayFftForm
             this.label1.TabIndex = 1;
             this.label1.Text = "Audio Device";
             // 
-            // formsPlot1
+            // transformedGraphPlot
             // 
-            this.transformedGraphPlot.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.transformedFormPlot.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.transformedGraphPlot.Location = new System.Drawing.Point(14, 60);
-            this.transformedGraphPlot.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
-            this.transformedGraphPlot.Name = "transformedGraphPlot";
-            this.transformedGraphPlot.Size = new System.Drawing.Size(905, 445);
-            this.transformedGraphPlot.TabIndex = 2;
+            this.transformedFormPlot.Location = new System.Drawing.Point(14, 445);
+            this.transformedFormPlot.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.transformedFormPlot.Name = "transformedFormPlot";
+            this.transformedFormPlot.Size = new System.Drawing.Size(905, 284);
+            this.transformedFormPlot.TabIndex = 2;
             // 
             // eventLoopTimer
             // 
             this.eventLoopTimer.Enabled = true;
-            this.eventLoopTimer.Interval = 50;
-            this.eventLoopTimer.Tick += new System.EventHandler(this.RunSingleIteration);
+            this.eventLoopTimer.Interval = 500;
+            this.eventLoopTimer.Tick += new System.EventHandler(this.RenderAll);
             // 
             // cbAutoAxis
             // 
@@ -98,14 +101,46 @@ partial class DisplayFftForm
             this.peakLabel.TabIndex = 9;
             this.peakLabel.Text = "Peak Frequency";
             // 
+            // windowTypeComboBox
+            // 
+            this.windowTypeComboBox.FormattingEnabled = true;
+            this.windowTypeComboBox.Location = new System.Drawing.Point(758, 29);
+            this.windowTypeComboBox.Name = "windowTypeComboBox";
+            this.windowTypeComboBox.Size = new System.Drawing.Size(140, 23);
+            this.windowTypeComboBox.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(674, 32);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 15);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Window Type";
+            // 
+            // original
+            // 
+            this.originalFormPlot.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.originalFormPlot.Location = new System.Drawing.Point(14, 117);
+            this.originalFormPlot.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.originalFormPlot.Name = "originalFormPlot";
+            this.originalFormPlot.Size = new System.Drawing.Size(905, 284);
+            this.originalFormPlot.TabIndex = 13;
+            // 
             // DisplayFftForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(933, 519);
+            this.ClientSize = new System.Drawing.Size(933, 743);
+            this.Controls.Add(this.originalFormPlot);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.windowTypeComboBox);
             this.Controls.Add(this.peakLabel);
             this.Controls.Add(this.cbAutoAxis);
-            this.Controls.Add(this.transformedGraphPlot);
+            this.Controls.Add(this.transformedFormPlot);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.devicesComboBox);
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
@@ -121,8 +156,11 @@ partial class DisplayFftForm
 
     private System.Windows.Forms.ComboBox devicesComboBox;
     private System.Windows.Forms.Label label1;
-    private ScottPlot.FormsPlot transformedGraphPlot;
+    private ScottPlot.FormsPlot transformedFormPlot;
     private System.Windows.Forms.Timer eventLoopTimer;
     private System.Windows.Forms.CheckBox cbAutoAxis;
     private System.Windows.Forms.Label peakLabel;
+    private System.Windows.Forms.ComboBox windowTypeComboBox;
+    private System.Windows.Forms.Label label2;
+    private ScottPlot.FormsPlot originalFormPlot;
 }
--- AudioWave.App/DisplayFftForm.cs
+++ AudioWave.App/DisplayFftForm.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows.Forms;
 using AudioWave.App.Devices;
+using FftSharp;
 using NAudio.Wave;
 using ScottPlot.Plottable;
 
@@ -9,13 +10,15 @@ namespace AudioWave.App;
 
 public partial class DisplayFftForm : Form
 {
+	private static readonly IReadOnlyCollection<IWindow> allWindows = Window.GetWindows();
 	private readonly IInputDevices inputDevices = new InputDevices();
 	private WaveInEvent currentAudioEvent;
-	
+
 	public DisplayFftForm()
 	{
 		InitializeComponent();
 		FillDevicesComboBox();
+		FillWindowTypeComboBox();
 	}
 
 	private void FillDevicesComboBox()
@@ -26,6 +29,12 @@ public partial class DisplayFftForm : Form
 
 		if (devicesComboBox.Items.Count == 0) MessageBox.Show("No any input devices found!");
 		else devicesComboBox.SelectedIndex = 0;
+	}
+
+	private void FillWindowTypeComboBox()
+	{
+		allWindows.ForEach(window => windowTypeComboBox.Items.Add(window));
+		windowTypeComboBox.SelectedIndex = 0;
 	}
 
 	private void ChangeInputDevice(object sender, EventArgs e)
@@ -47,41 +56,74 @@ public partial class DisplayFftForm : Form
 			lastBuffer[i] = BitConverter.ToInt16(args.Buffer, i * bytesPerSample);
 	}
 
-	private SignalPlot signalPlot;
+	private void RenderAll(object sender, EventArgs e)
+	{
+		RenderOriginalSignal();
+		RenderTransformedSignal();
+	}
 
-	private void RunSingleIteration(object sender, EventArgs e)
+	private SignalPlot originalPlot;
+
+	private void RenderOriginalSignal()
 	{
 		if (lastBuffer is null)
 		{
 			return;
 		}
 
-		var window = new FftSharp.Windows.Hanning();
+		if (!originalFormPlot.Plot.GetPlottables().Any())
+		{
+			originalPlot = originalFormPlot.Plot.AddSignal(lastBuffer.Select(Math.Log10).ToArray());
+		}
+		else
+		{
+			originalPlot.Ys = lastBuffer;
+		}
+
+		originalFormPlot.Plot.XLabel("Time");
+		originalFormPlot.Plot.YLabel("Signal amplitude");
+
+		originalFormPlot.Plot.AxisAuto(horizontalMargin: 0);
+		originalFormPlot.Render();
+	}
+
+	private SignalPlot transformedPlot;
+
+	private void RenderTransformedSignal()
+	{
+		if (lastBuffer is null)
+		{
+			return;
+		}
+
+		var window = allWindows.ElementAt(windowTypeComboBox.SelectedIndex);
 		double[] windowed = window.Apply(lastBuffer);
-		double[] zeroPadded = FftSharp.Pad.ZeroPad(windowed);
-		double[] fftPower = FftSharp.Transform.FFTpower(zeroPadded);
-		double[] fftFreq = FftSharp.Transform.FFTfreq(currentAudioEvent.WaveFormat.SampleRate, fftPower.Length);
+		//double[] windowed = lastBuffer;
+		double[] zeroPadded = Pad.ZeroPad(windowed);
+		double[] fftPower = Transform.FFTpower(zeroPadded);
+		double[] fftFreq = Transform.FFTfreq(currentAudioEvent.WaveFormat.SampleRate, fftPower.Length);
 
 		peakLabel.Text = $"Peak Frequency: {PeakFrequency(fftPower, fftFreq):N0} Hz";
-		transformedGraphPlot.Plot.XLabel("Frequency Hz");
-		transformedGraphPlot.Plot.YLabel("Decibels");
+		transformedFormPlot.Plot.XLabel("Frequency Hz");
+		transformedFormPlot.Plot.YLabel("Decibels");
 
-		if (!transformedGraphPlot.Plot.GetPlottables().Any())
+
+		if (!transformedFormPlot.Plot.GetPlottables().Any())
 		{
-			signalPlot = transformedGraphPlot.Plot.AddSignal(
+			transformedPlot = transformedFormPlot.Plot.AddSignal(
 				fftPower,
 				2.0 * fftPower.Length / currentAudioEvent.WaveFormat.SampleRate);
 		}
 		else
 		{
-			signalPlot.Ys = fftPower;
+			transformedPlot.Ys = fftPower;
 		}
 
 		if (cbAutoAxis.Checked)
 		{
 			try
 			{
-				transformedGraphPlot.Plot.AxisAuto(horizontalMargin: 0);
+				transformedFormPlot.Plot.AxisAuto(horizontalMargin: 0);
 			}
 			catch (Exception ex)
 			{
@@ -91,7 +133,7 @@ public partial class DisplayFftForm : Form
 
 		try
 		{
-			transformedGraphPlot.Render();
+			transformedFormPlot.Render();
 		}
 		catch (Exception ex)
 		{
