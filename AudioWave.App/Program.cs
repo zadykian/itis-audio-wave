12093e097cff1b4b6c1513c118be618153b30afe
zadykian <zadykian@outlook.com>
2022-06-07 20:34:36 +0300

    AudioWave.App

---
+++ AudioWave.App/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AudioWave.App;
+
+/// <summary>
+/// Application entry point. 
+/// </summary>
+internal static class Program
+{
+	/// <summary>
+	/// Entry point method. 
+	/// </summary>
+	private static Task Main() => Task.CompletedTask;
+}
